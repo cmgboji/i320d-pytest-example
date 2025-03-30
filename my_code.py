ed1900329d83798a881e00e89ad528717f62c3cc
Chinonye Mgboji <chinonye.mgboji@utexas.edu>
2025-03-29 20:06:00 -0500

    Revised fix_phone_num to pass tests

--- my_code.py
+++ my_code.py
@@ -1,10 +1,17 @@
 import pytest
 
 def fix_phone_num(phone_num_to_fix):
-  # given "5125558823". Split the parts, then recombine and return
-  area_code = phone_num_to_fix[0:3] # 512 (first three digits)
-  three_part = phone_num_to_fix[3:6] # 555 (next three digits)
-  four_part = phone_num_to_fix[6:] # # 8823 (last four digits)
+  cleaned_phone_num = ''
+  for num in phone_num_to_fix:
+        if num.isdigit():
+            cleaned_phone_num += num
+            
+  if len(cleaned_phone_num) != 10:
+      raise ValueError("Invalid Phone Number Length")
+        
+  area_code = cleaned_phone_num[0:3] # 512 (first three digits)
+  three_part = cleaned_phone_num[3:6] # 555 (next three digits)
+  four_part = cleaned_phone_num[6:] # # 8823 (last four digits)
   
   fixed_num = "(" + area_code + ")" + " " + three_part + " " + four_part 
   
@@ -20,11 +27,9 @@ def test_error_phone_nums():
   assert fix_phone_num('(321) 654 3333') == '(321) 654 3333'
     
 def test_value_error_check():
-    phone_number = fix_phone_num('51')
-    if len(phone_number) != 10:
-        raise ValueError("Invalid phone number length")
+    with pytest.raises(ValueError):
+        fix_phone_num('51')
 
 def test_not_all_digits():
-    phone_number = fix_phone_num("bjjlnvxyi")
-    if not phone_number.isdigit():
-        raise ValueError("Can only include numbers")
+    with pytest.raises(ValueError):
+        fix_phone_num("bjjlnvxyi")
